f8803bb84f9f7e2b6fb26b01ee0b005941ac9bc4
nephydecode <nephydecode@gmail.com>
2024-03-12 22:28:53 +0800

    Updates

---
+++ precache-manifest.2fcc7c733426747105b1915b9fcf6a9a.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "01336b3348dee64bcb93",
+    "url": "/react-portfolio-2020/static/css/main.64eef375.chunk.css"
+  },
+  {
+    "revision": "01336b3348dee64bcb93",
+    "url": "/react-portfolio-2020/static/js/main.01336b33.chunk.js"
+  },
+  {
+    "revision": "f9e8e3fc9cb1fc8ef5c0",
+    "url": "/react-portfolio-2020/static/css/1.ca38b747.chunk.css"
+  },
+  {
+    "revision": "f9e8e3fc9cb1fc8ef5c0",
+    "url": "/react-portfolio-2020/static/js/1.f9e8e3fc.chunk.js"
+  },
+  {
+    "revision": "6c479cc97f498e2c996c",
+    "url": "/react-portfolio-2020/static/js/runtime~main.6c479cc9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-portfolio-2020/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "cecff9ae6a36fc97729db282e8a63719",
+    "url": "/react-portfolio-2020/static/media/webportfolio-banner.cecff9ae.png"
+  },
+  {
+    "revision": "82eaa68530a0177257e82cdae4aba250",
+    "url": "/react-portfolio-2020/static/media/arrow-down.82eaa685.svg"
+  },
+  {
+    "revision": "1f02e7fd6ce954bf31badaa1684acd57",
+    "url": "/react-portfolio-2020/static/media/dev-icon.1f02e7fd.svg"
+  },
+  {
+    "revision": "0c12dba71409077d8dfed994e9400274",
+    "url": "/react-portfolio-2020/static/media/xr-icon.0c12dba7.svg"
+  },
+  {
+    "revision": "9e53551ac33f995ca6690e99ca2c5b39",
+    "url": "/react-portfolio-2020/static/media/angular-icon.9e53551a.svg"
+  },
+  {
+    "revision": "0607eb99d5a4a1a3632a5684f5441fc2",
+    "url": "/react-portfolio-2020/static/media/maya-icon.0607eb99.svg"
+  },
+  {
+    "revision": "790800da04a193aeda4c26ba6f3a5908",
+    "url": "/react-portfolio-2020/static/media/react-icon.790800da.svg"
+  },
+  {
+    "revision": "dc13619789cc40044404de0af808ff98",
+    "url": "/react-portfolio-2020/static/media/sketch-icon.dc136197.svg"
+  },
+  {
+    "revision": "6bddefeed621d227766bc586cd6056aa",
+    "url": "/react-portfolio-2020/static/media/unity-icon.6bddefee.svg"
+  },
+  {
+    "revision": "f0da8f9b8514a861b375ac7b4b249795",
+    "url": "/react-portfolio-2020/static/media/zenject-icon.f0da8f9b.svg"
+  },
+  {
+    "revision": "6ec58d1ea836b7ef69f94ea6770972a3",
+    "url": "/react-portfolio-2020/static/media/Raleway-SemiBold.6ec58d1e.ttf"
+  },
+  {
+    "revision": "84abe14c9756256a4b91300ba3e4ec62",
+    "url": "/react-portfolio-2020/static/media/Raleway-Regular.84abe14c.ttf"
+  },
+  {
+    "revision": "525d84c20770933217edbb52582a342e",
+    "url": "/react-portfolio-2020/index.html"
+  }
+];
